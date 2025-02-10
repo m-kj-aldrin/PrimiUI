2bb8a8e12485209e6b1bd8e7cb6bc48b7c432895
m-kj-aldrin <mkj.aldrin@icloud.com>
2025-02-10 16:01:36 +0100

    Consolidate Select component styles into a single base CSS file

--- test/index.js
+++ test/index.js
@@ -1,14 +1,6 @@
 document.addEventListener("change", (event) => {
     if (!(event instanceof CustomEvent)) return;
-    try {
-        const { value } = event.detail;
-        let selectRoots = document.querySelectorAll("select-root");
 
-        for (let selectRoot of selectRoots) {
-            if (selectRoot == event.target) continue;
-            selectRoot.setAttribute("value", value);
-        }
-    } catch (error) {
-        console.error(error);
-    }
+    const detail = event.detail;
+    console.log(detail);
 });
